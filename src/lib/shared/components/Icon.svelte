a793d93730cd9dba5c3967c427a7d744893bb650
Raphael Schütz <70175424+Raphael2b3@users.noreply.github.com>
2025-03-05 20:10:19 +0100

    refactor Icon component to support additional class names

--- src/lib/shared/components/Icon.svelte
+++ src/lib/shared/components/Icon.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import Icon, { type IconProps } from '@iconify/svelte';
 	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { cn } from '../utils';
 	let { children, ...props }: IconProps & SvelteHTMLElements['svg'] & { children?: any } = $props();
 </script>
 
-<div class="flex flex-row gap-1">
+<div class="{cn('flex flex-row gap-1', props.class)}}">
 	<Icon height="1.2rem" width="1.2rem" {...props}></Icon>
 	{@render children?.()}
 </div>
